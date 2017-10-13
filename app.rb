518d6f476961fdea89673c2dd4669508dd05126e
Christine Tran <christinetran825@gmail.com>
2017-10-13 21:22:37 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,7 +8,21 @@ class App < Sinatra::Base
   end
 
   post '/team' do
-    @team_position = params[""]
+    @team_name = params[":team_name"]
+    @team_name = params[":coach"]
+    @team_name = params[":pg"]
+    @team_name = params[":sg"]
+    @team_name = params[":pf"]
+    @team_name = params[":sf"]
+    @team_name = params[":c"]
+    <input type="text" name="">
+    <input type="text" name="">
+    <input type="text" name="">
+    <input type="text" name="">
+    <input type="text" name="">
+    <input type="text" name="">
+    <input type="text" name="">
+    <input type="submit">
     erb :team
   end
 
